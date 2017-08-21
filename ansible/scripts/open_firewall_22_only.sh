b9fbc5d036e511db7030f03639a7f420e44c67b7
Bill Tooles <btooles@gmail.com>
2017-08-20 20:25:35 -0400

    Added the 'open_firewall*' scripts to open and close port 80 while keeping port 22 open always

---
+++ ansible/scripts/open_firewall_22_only.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+IPT=/sbin/iptables
+
+$IPT -F
+
+# policies
+
+$IPT -P OUTPUT ACCEPT
+$IPT -P INPUT DROP
+$IPT -P FORWARD DROP
+$IPT -N SERVICES
+
+# allowed inputs
+
+$IPT -A INPUT --in-interface lo -j ACCEPT
+$IPT -A INPUT -j SERVICES
+
+# allow responses
+
+$IPT -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# allowed services
+
+$IPT -A SERVICES -p tcp --dport 22 -j ACCEPT
